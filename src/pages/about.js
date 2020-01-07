2c1cd872560dd8e51feac4c0539342b270badd2a
Diana Grams <39970616+deegrams221@users.noreply.github.com>
2020-01-06 18:35:44 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ export default ({ data }) => (
   <Layout>
     <h1>About {data.site.siteMetadata.title}</h1>
     <p>
-      This blog was created as part of the #100DaysOfGatsby Challenge on Twitter. Creating a Gatsby blog from scratch was challenge 1. Over teh last week, I have gone through all Gatsby's introduction tutorials and put this blog togther. I also played a bit with iframe, which you will be able to see in my first blog post entitled "Blogging".
+      This blog was created as part of the #100DaysOfGatsby Challenge on Twitter. Creating a Gatsby blog from scratch was challenge 1. Over the last week, I have gone through all Gatsby's introduction tutorials and put this blog togther. I also played a bit with iframe, which you will be able to see in my first blog post entitled "Blogging".
     </p>
   </Layout>
 )
